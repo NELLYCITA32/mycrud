6f0602c6334df95b259082b7666c2c3723a927cc
NELLYCITA32 <nellyvallejomuiba@gmail.com>
2021-11-10 14:03:32 -0400

    Create mycrud.php

---
+++ mycrud.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: MYCRUD
+* Plugin URI: https://loginweb.dev/MYCRUD/
+* Description: Funciones basicas acciones para interactuar en la base de datos.
+* Version: 1.0
+* Author: Ing. Nelly Vallejo Muiba
+* Author URI: https://loginweb.dev/profile
+**/
+?>
